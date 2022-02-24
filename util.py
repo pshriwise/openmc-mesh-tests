c929d0c9eb145f1dd418e0fbc4aeac6c86634022
Patrick Shriwise <pshriwise@gmail.com>
2022-02-23 21:32:31 -0600

    Improving tmp dir decorator

--- util.py
+++ util.py
@@ -1,12 +1,13 @@
-
 import os
+import functools
 import tempfile
 
 
 def run_in_tmpdir(f):
-    def inner():
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
         with tempfile.TemporaryDirectory() as tmpdir:
             os.chdir(tmpdir)
-            f()
+            f(*args, **kwargs)
     return inner
 
